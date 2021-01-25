d845178e79cfdbed0b854e828fc1b4b1474af795
asmaatalha <77047778+asmaatalha@users.noreply.github.com>
2021-01-25 20:10:50 +0100

    Add files via upload

---
+++ C_workshop/step1_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Nom %s\n", "asmaa");
+    printf("Prenom %s\n", "talha");
+    printf("Age %d\n", 22);
+    printf("Sexe %s\n", "femme");
+
+    return 0;
+}
